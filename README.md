90b2a21fd7cdfbd093e442ad3cff965634ac253c
Stefanus Adhie <stefanusadhie@Stefanuss-MacBook-Pro.local>
2022-11-09 09:03:30 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # MariaDB
+
+## Install
+
+```
+npm i -s @stefanusadhie/mariadb
+```
+
+## Create Connection
+
+```
+const mariadb = require("@stefanusadhie/mariadb");
+
+const mariadb_class = mariadb({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "tradepro",
+  max_reconnect: 2,
+  timeout_reconnect: 1000,
+});
+
+mariadb_class.createConnection();
+```
--- example/index.js
+++ example/index.js
@@ -1,13 +1,18 @@
 const mariadb = require("@stefanusadhie/mariadb");
 
-const mariadb_class = mariadb({
+const connection = mariadb({
   host: "localhost",
-  port: 3306,
+  port: "3306",
   user: "root",
-  password: "asdf1234*",
-  database: "tradepro",
+  password: "",
+  // database: "tradepro",
   max_reconnect: 2,
   timeout_reconnect: 1000,
 });
 
-mariadb_class.createConnection();
+(async () => {
+  const ping = await connection.ping();
+  // return "PONG"
+
+  const connected = await connection.createConnection();
+})();
