83802849850c656cb9050d4832065d12ee4cb2b3
Stefanus Adhie <stefanus.adhie@inovasisolusi.com>
2022-12-05 14:54:12 +0700

    update class

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const connection = mariadb({
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "",
+  password: "asdf1234*",
   // database: "tradepro",
   max_reconnect: 2,
   timeout_reconnect: 1000,
@@ -15,4 +15,34 @@ const connection = mariadb({
   // return "PONG"
 
   const connected = await connection.createConnection();
+
+  setInterval(async () => {
+    try {
+      const query = await connection.query("select host,user from mysql.user");
+      console.log({ query });
+    } catch (error) {
+      console.log({ error });
+    }
+  }, 5000);
 })();
+
+// process.stdin.resume(); //so the program will not close instantly
+
+// function exitHandler(options, exitCode) {
+//   if (options.cleanup) console.log("clean");
+//   if (exitCode || exitCode === 0) console.log(exitCode);
+//   if (options.exit) process.exit();
+// }
+
+// //do something when app is closing
+// process.on("exit", exitHandler.bind(null, { cleanup: true }));
+
+// //catches ctrl+c event
+// process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+
+// // catches "kill pid" (for example: nodemon restart)
+// process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+// process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
+
+// //catches uncaught exceptions
+// process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
